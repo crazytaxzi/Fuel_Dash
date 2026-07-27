42fc627065536ad24606fb059a5b69f901e0dc4e
Sentionce <crazytaxzi@gmail.com>
2026-07-27 05:30:04 -0700

    Load Worked workflow module

--- serve_dashboard.ps1
+++ serve_dashboard.ps1
@@ -78,6 +78,9 @@ function Get-ServedFileBytes([string]$Candidate) {
         if ($Html -notmatch 'missing_bol_driver_only\.js') {
             $ScriptTags += '<script src="missing_bol_driver_only.js"></script>'
         }
+        if ($Html -notmatch 'worked_workflow\.js') {
+            $ScriptTags += '<script src="worked_workflow.js"></script>'
+        }
         $ScriptTags += '<script src="app.js"></script>'
         $Replacement = $ScriptTags -join ("`r`n  ")
         $Html = $Html.Replace('<script src="app.js"></script>', $Replacement)
