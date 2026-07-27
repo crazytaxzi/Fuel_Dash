c68617351cc67a75ba22b67d4c6b2ea73e03ee6e
Sentionce <crazytaxzi@gmail.com>
2026-07-27 09:23:30 -0700

    Load per-note transition toggle module

--- serve_dashboard.ps1
+++ serve_dashboard.ps1
@@ -81,6 +81,9 @@ function Get-ServedFileBytes([string]$Candidate) {
         if ($Html -notmatch 'worked_workflow\.js') {
             $ScriptTags += '<script src="worked_workflow.js"></script>'
         }
+        if ($Html -notmatch 'note_transition_toggle\.js') {
+            $ScriptTags += '<script src="note_transition_toggle.js"></script>'
+        }
         if ($Html -notmatch 'transition_export_v2\.js') {
             $ScriptTags += '<script src="transition_export_v2.js"></script>'
         }
